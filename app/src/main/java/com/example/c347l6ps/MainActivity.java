35966917004de4ae0ecbdfacf24dffcbcf285722
19047241 <19047241@rp.edu.sg>
2021-06-01 16:01:02 +0800

    Haziq

--- app/src/main/java/com/example/c347l6ps/MainActivity.java
+++ app/src/main/java/com/example/c347l6ps/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
+import com.bumptech.glide.Glide;
 import com.example.c347l6ps.Fragments.CnnRssFragment;
 import com.example.c347l6ps.Fragments.Fact1Fragment;
 import com.example.c347l6ps.Fragments.Fact2Fragment;
@@ -31,13 +40,24 @@ public class MainActivity extends AppCompatActivity {
 //    ArrayList<String[]> factsArray;
 
 
+    private ImageView ivTrivia;
+    // Shared preferences for screen state
+    private String activeFrag;
+
+    public static final String SHARED_SCREEN = "lastActiveScreen";
+    private Button btnReadLater;
+
+    FragmentManager fm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        btnReadLater = findViewById(R.id.btnReadLater);
         viewPager = findViewById(R.id.viewPager);
-        FragmentManager fm = getSupportFragmentManager();
+        fm = getSupportFragmentManager();
         Resources res = getResources();
 
 //        facts1 = res.getStringArray(R.array.facts);
@@ -52,8 +72,20 @@ public class MainActivity extends AppCompatActivity {
         al.add(new Fact2Fragment());
         al.add(new CnnRssFragment());
 
+        loadSharedPrefs();
+        updateFragmentOnStart();
+
         adapter = new MyFragmentPageAdapter(fm, al);
         viewPager.setAdapter(adapter);
+
+        btnReadLater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSharedPrefs();
+            }
+        });
+
+        Glide.with(this).load("https://otogi.wikiru.jp/attach2/A1FA352FA5CFA5E0A5ECA5C3A5C8_53445FA5CFA5E0A5ECA5C3A5C8305F766963746F72792E676966.gif").into(ivTrivia);
 
     }
 
@@ -84,14 +116,13 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("numbers", String.valueOf(random));
                 if (random == 1) {
                     int max2 = viewPager.getChildCount();
-                    if (viewPager.getCurrentItem() < max2 -1) {
+                    if (viewPager.getCurrentItem() < max2 - 1) {
                         int nextPage = viewPager.getCurrentItem() + 1;
                         viewPager.setCurrentItem(nextPage, true);
                         return true;
 
                     }
-                }
-                else if (random == 2){
+                } else if (random == 2) {
                     if (viewPager.getCurrentItem() > 0) {
                         int previousPage = viewPager.getCurrentItem() - 1;
                         viewPager.setCurrentItem(previousPage, true);
@@ -101,4 +132,40 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-        }}
+    }
+
+
+
+    public void saveSharedPrefs(){
+        setCurrentFragment();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_SCREEN, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(SHARED_SCREEN, activeFrag);
+
+        editor.apply();
+    }
+
+    public void loadSharedPrefs(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_SCREEN, MODE_PRIVATE);
+
+        activeFrag = sharedPreferences.getString(SHARED_SCREEN, "");
+    }
+
+    public void updateFragmentOnStart(){
+        if(activeFrag.equalsIgnoreCase("frag1")){
+            viewPager.setCurrentItem(1, true);
+        }else{
+            viewPager.setCurrentItem(2, true);
+        }
+    }
+
+    public void setCurrentFragment() {
+        Fragment currentFrag = fm.findFragmentById(R.id.container);
+        if(currentFrag == al.get(0)){
+            activeFrag = "frag1";
+        }else{
+            activeFrag = "frag2";
+        }
+    }
+}
